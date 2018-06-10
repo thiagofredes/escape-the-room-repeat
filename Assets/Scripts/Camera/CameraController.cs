73d4a35aeec7fd368265b1985e907facff388185
Thiago <trodrigues02@gmail.com>
2018-06-10 16:30:57 -0300

    Added basic movement and camera functionalities

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float distanceToPlayer;
+
+	private PlayerController _player;
+
+	private Vector3 _cameraOrbit;
+
+
+	void Awake(){
+		_player = FindObjectOfType<PlayerController>();		
+	}
+
+	void Start(){
+		SetCameraOrbit();
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 speed = Vector3.zero;
+		this.transform.position = _player.transform.position + _cameraOrbit * distanceToPlayer;
+		this.transform.LookAt(_player.transform.position);
+	}
+
+	private void SetCameraOrbit(){
+		_cameraOrbit = this.transform.position - _player.transform.position;
+		_cameraOrbit.Normalize();
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public CharacterController characterController;
+
+	public float movementSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(horizontal, 0f, vertical);
+
+		characterController.Move(movement * Time.deltaTime * movementSpeed);
+	}
+}
