8ec930f04cdc62c8a06cf928c9c589b4a319c357
Thiago <trodrigues02@gmail.com>
2018-06-16 11:54:43 -0300

    Created simple shooting mechanics

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,11 +8,18 @@ public class PlayerController : MonoBehaviour {
 
 	public float movementSpeed;
 
+	public PlayerShotController shotController;
+
+
 	// Update is called once per frame
 	void Update () {
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical = Input.GetAxis("Vertical");
-		Vector3 movement = new Vector3(horizontal, 0f, vertical);
+		Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
+
+		if(Input.GetKey(KeyCode.Space)){
+			shotController.Shoot();
+		}
 
 		characterController.Move(movement * Time.deltaTime * movementSpeed);
 	}
